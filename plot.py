3ace1b005076902d5ef90142f2c96d086bcb4cb0
Chris LeBailly <chris.lebailly@gmail.com>
2014-09-12 00:09:52 -0700

    Expanded functionality

--- plot.py
+++ plot.py
@@ -6,33 +6,61 @@
 from __future__ import print_function
 import matplotlib.pyplot as plt
 import sys, argparse
+from itertools import izip
+from matplotlib.backends.backend_pdf import PdfPages
 
 def main():
 	"""
+	Parses arguments, plots point (expand).
 	"""
-
-	plt.show()
 
 	args = parse_arguments()
 
 	#Read in header, decided how to precide based on header length!
 	header = sys.stdin.readline().strip().split(',')
-	x_index = header.index(args.x)
-	y_index = header.index(args.y)
+	x_indices = [header.index(i) for i in args.x]
+	y_indices = [header.index(i) for i in args.y]
 
 	for line in sys.stdin:
 		point = line.split(',')
-		plt.plot(point[x_index],point[y_index],'b.')
+		for x_index, y_index, color in izip(x_indices, y_indices, args.colors):
+			plt.plot(point[x_index],point[y_index], color)
 
-	plt.show()
+	if(args.xmax is not None): plt.xlim(xmax = args.xmax)
+	if(args.xmin is not None): plt.xlim(xmin = args.xmin)
+	if(args.ymax is not None): plt.ylim(ymax = args.ymax)
+	if(args.ymin is not None): plt.ylim(ymin = args.ymin)
+
+	if(args.output is None): plt.show()
+	else: plt.savefig(args.output, format='pdf')
 
 def parse_arguments():
 	""" Parses arguments from the command line """
 
 	parser = argparse.ArgumentParser(description = __doc__)
 
-	parser.add_argument('-x', type=str,default='Position')
-	parser.add_argument('-y', type=str,default='Velocity')
+	parser.add_argument('-x', type=str, nargs='+', default=['Position'])
+	parser.add_argument('-y', type=str, nargs='+', default=['Velocity'])
+	parser.add_argument('--colors', '-c', type=str, nargs='+', default=['b.'])
+
+	#Axis limit parameters
+	parser.add_argument('--xmax', type=float, default=None)
+	parser.add_argument('--xmin', type=float, default=None)
+	parser.add_argument('--ymax', type=float, default=None)
+	parser.add_argument('--ymin', type=float, default=None)
+
+	#ATTEN - add option to do axis limits all at once. Mutually exclusive group?
+	#ATTEN - Add option to connect the dots (if possible)
+	#ATTEN - Image size parameters
+
+	#Labels - x-axis, y-axis, and plot title
+	parser.add_argument('--xlab', type=str, default=None)
+	parser.add_argument('--ylab', type=str, default=None)
+	parser.add_argument('--title', type=str, default=None)
+	#http://stackoverflow.com/questions/12444716/how-do-i-set-figure-title-and-axes-labels-font-size-in-matplotlib
+
+	#Output results
+	parser.add_argument('--output','-o', type=str, default=None)
 
 	return parser.parse_args()
 
